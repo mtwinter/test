6b5ef1058280abe45cbc319bc44b9538b5f4cb49
Max_Winter <=>
2018-05-09 13:54:07 -0400

    i made code

---
+++ src/git_demo/Main.java
@@ -0,0 +1,14 @@
+package git_demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//change
+		
+		public void empty() {
+			
+		}
+	}
+
+}
